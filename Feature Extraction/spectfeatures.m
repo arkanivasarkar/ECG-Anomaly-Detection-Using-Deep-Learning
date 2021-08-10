6efde6b2c6f701905865c50ffc9f76d066049aa2
Arkaniva Sarkar <73361480+darkbiologist@users.noreply.github.com>
2021-08-11 01:41:25 +0530

    Add files via upload

---
+++ Feature Extraction/spectfeatures.m
@@ -0,0 +1,24 @@
+function pout = spectfeatures(ecg,count)
+
+fs = 250;
+
+ifq = instfreq(ecg,fs);
+ifq = (ifq)';
+ifq=repmat(ifq,1,36);
+ifq=ifq(1:9000);
+
+se = pentropy(ecg,fs);
+se = (se)';
+se=repmat(se,1,36);
+se=se(1:9000);
+
+switch count
+    case 1
+        pout = ifq;
+    case 2
+        pout = se;
+end
+
+end
+
+
